cc45ee2e4ecc4824ec8e9b46a4fd7783d077a81c
gogiari <gomul82@gmail.com>
2023-09-23 16:09:45 +0900

    로그인작업중

--- src/main/java/com/gogiari/myproject/user/controller/UserController.java
+++ src/main/java/com/gogiari/myproject/user/controller/UserController.java
@@ -2,9 +2,14 @@ package com.gogiari.myproject.user.controller;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
+
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -41,6 +46,26 @@ public class UserController {
         // System.out.println(userEntity);
         userRepository.save(userEntity);
         
+        return mv;
+    }
+
+    @GetMapping("/login")
+    public String loginForm() {
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public ModelAndView login(HttpSession session, String userid, String password) {
+        ModelAndView mv = new ModelAndView("redirect:/");
+        Optional<UserEntity> findUser = userRepository.findById(userid);
+        UserEntity user = findUser.orElse(null);
+
+        if(user.getPassword().equals(password)){ // 아이디 잘못햇을때 동작안함수정해야함!@!!
+            session.setAttribute("userid", userid);
+        } else {
+            mv.setViewName("/login");
+            mv.addObject("message", "아이디나 비밀번호를 다시 확인해주세요.");
+        }
         return mv;
     }
 }
